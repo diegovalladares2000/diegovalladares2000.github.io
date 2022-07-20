7a94a1dc8e1e3dd3a42be396d876ea82bef031eb
diegovalladares2000 <103965791+diegovalladares2000@users.noreply.github.com>
2022-07-19 20:56:24 -0400

    Add files via upload

---
+++ js/evento_jquery.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+  $("#sumar").click(
+      function(){
+          var num1 = parseInt($("#num1").val());
+          var num2 = parseInt($("#num2").val());
+          var suma = num1+num2
+          
+          $("#resultado").val(suma)
+      }
+  );
+}
+);
+
+
+
+
+$(document).ready(function(){
+  $("#sumar").click(function(){$("#sumar").hide(1500)})
+  
+});
+
+
+
---
+++ js/eventos.js
@@ -0,0 +1,13 @@
+
+
+ function sumar() {
+    // capturamos los valores ingresados
+    var num1 = document.getElementById("num1").value
+    var num2 = document.getElementById("num2").value
+  
+   suma = parseInt(num1)+parseInt(num2)
+   res = document.getElementById("resultado")
+    res.value = suma
+  }
+  
+  
